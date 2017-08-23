d6cba02b823b047a8ed29af5e14e9e0fc7e04ea3
Nemo <zhhjchina@gmail.com>
2017-08-23 16:32:39 +0800

    Fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import OneSizeContainer from './component.vue'
 export default {
   install: function (Vue) {
     Vue.component('OneSizeContainer', OneSizeContainer)
-  }
+  },
+  OneSizeContainer
 }
-
-export OneSizeContainer
